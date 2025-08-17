3207ba70e83af053c0fd89631b75815e026e850f
Christopher J. Bottaro <cjb@cjbotta.ro>
2025-08-17 16:27:01 -0500

    Tweaks for macOS, add starship.toml

--- setup.sh
+++ setup.sh
@@ -2,21 +2,25 @@
 set -e
 
 if [[ "$(uname)" == "Linux" ]]; then
+  FONT_DIR=~/.local/share/fonts/
+
   sudo cp keyd.conf /etc/keyd/default.conf
   sudo systemctl enable keyd
   sudo systemctl restart keyd
-
-  if [ ! -f "$HOME/.local/share/fonts/BitstromWeraNerdFontMono-Bold.ttf" ]; then
-    wget -q -P ~/Downloads https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/BitstreamVeraSansMono.zip
-    rm -rf ~/Downloads/BitstreamVeraSansMono
-    unzip -qo ~/Downloads/BitstreamVeraSansMono.zip -d ~/Downloads/BitstreamVeraSansMono
-    mv ~/Downloads/BitstreamVeraSansMono/BitstromWeraNerdFontMono-Bold.ttf ~/.local/share/fonts/
-    mv ~/Downloads/BitstreamVeraSansMono/BitstromWeraNerdFontMono-Regular.ttf ~/.local/share/fonts/
-    rm -rf ~/Downloads/BitstreamVeraSansMono*
-    fc-cache ~/.local/share/fonts/
-  fi
 fi
 
 if [[ "$(uname)" == "Darwin" ]]; then
-  
+  FONT_DIR=~/Library/Fonts
+fi
+
+if [ ! -f "$FONT_DIR/.local/share/fonts/BitstromWeraNerdFontMono-Bold.ttf" ]; then
+  wget -q -P ~/Downloads https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/BitstreamVeraSansMono.zip
+  rm -rf ~/Downloads/BitstreamVeraSansMono
+  unzip -qo ~/Downloads/BitstreamVeraSansMono.zip -d ~/Downloads/BitstreamVeraSansMono
+  mv ~/Downloads/BitstreamVeraSansMono/BitstromWeraNerdFontMono-Bold.ttf $FONT_DIR
+  mv ~/Downloads/BitstreamVeraSansMono/BitstromWeraNerdFontMono-Regular.ttf $FONT_DIR
+  rm -rf ~/Downloads/BitstreamVeraSansMono*
+  if [[ "$(uname)" == "Linux" ]]; then
+    fc-cache $FONT_DIR
+  fi
 fi
